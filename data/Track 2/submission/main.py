e99f23c4aa3f54a881a6b1068610e05ac848f142
ayunimatulf <ayunimatulfitriyah369@gmail.com>
2020-02-08 17:14:07 +0700

    fix all

--- data/Track 2/submission/main.py
+++ data/Track 2/submission/main.py
@@ -1,8 +1,19 @@
 import pandas as pd
 
-test = pd.read_csv("test.csv")
-
+toc=time.time()
+test_mydata = pd.read_csv("test_mydata.csv")
+print(test_mydata.head())
+train_stats=pd.read_csv('train_stats.csv')
+print(train_stats.head())
+def norm(x):
+    return (x - train_stats['mean']) / train_stats['std']
+label=["id", "x", "y", "z", "Vx", "Vy", "Vz"]
+normed_test_data = norm(test_mydata.drop(label, axis=1))
+tic=time.time()
+print(tic-toc)
+# print(test_mydata.head)
+print(normed_test_data.head())
 # just sending simulated values as the answer
-submission = test[["id", "x_sim", "y_sim", "z_sim", "Vx_sim", "Vy_sim", "Vz_sim"]]
-submission.columns = ["id", "x", "y", "z", "Vx", "Vy", "Vz"]
-submission.to_csv("submission.csv", index=False)
+# submission = test[["id", "x_sim", "y_sim", "z_sim", "Vx_sim", "Vy_sim", "Vz_sim"]]
+# submission.columns = ["id", "x", "y", "z", "Vx", "Vy", "Vz"]
+# submission.to_csv("submission.csv", index=False)
--- track2/submission/main.py
+++ track2/submission/main.py
@@ -1,8 +1,30 @@
 import pandas as pd
+from tensorflow.keras import models
+import time
 
-test = pd.read_csv("test.csv")
 
+#data stats for normalization
+df_stats=pd.read_csv('train_stats.csv', index_col=0)
+df_stats=df_stats.drop('sat_id')
+
+#data variable
+list_variable={'x':['x_sim', 'z_sim', 'Vy_sim'],
+         'y': ['x_sim', 'y_sim', 'Vz_sim'],
+         'z': ['x_sim', 'z_sim', 'Vy_sim', 'Vz_sim'],
+         'Vx': ['y_sim', 'Vx_sim', 'Vz_sim'],
+         'Vy': ['z_sim', 'Vx_sim', 'Vy_sim', 'Vz_sim'],
+         'Vz': ['y_sim', 'z_sim', 'Vy_sim', 'Vz_sim']}
+
+#normalization
+def norm(x):
+    return (x - df_stats['mean']) / df_stats['std']
+test = pd.read_csv("/Users/bytedance-it138302/Documents/IDAO2020/test_2.csv")
 # just sending simulated values as the answer
 submission = test[["id", "x_sim", "y_sim", "z_sim", "Vx_sim", "Vy_sim", "Vz_sim"]]
-submission.columns = ["id", "x", "y", "z", "Vx", "Vy", "Vz"]
-submission.to_csv("submission.csv", index=False)
+submission=submission.set_index('id')
+normed_data=norm(submission)
+for i in list_variable:
+    model=models.load_model('model/8_model_'+i+'.h5')
+    submission[i]=model.predict(normed_data[list_variable[i]].values)
+submission = submission[["x", "y", "z", "Vx", "Vy", "Vz"]]
+submission.to_csv("submission.csv", index=True)
